1c19da7a4226fe4d66d11db994592b6ffe6d8532
Eric Santos <eric.silvasantos44@gmail.com>
2022-07-31 19:08:16 -0300

    fix: quinto requisito

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const StrangerThingsService = require('./services/StrangerThings');
 
 const app = express();
 
-// const PORT = process.env.PORT || 3000;
 const { UPSIDEDOWN_MODE, PORT } = process.env;
 
 const strangerThingsRepository = new StrangerThingsRepository(
